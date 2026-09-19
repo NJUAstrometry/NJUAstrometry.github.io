b8d384c520978eb2b639a23d662f721f1d7b292d
neo <niu.liu@nju.edu.cn>
2026-09-19 21:45:56 +0800

    chore: remove debug commands from run.sh
    - Dropped rg search and git diff checks
    - Cleaned up unused verification snippets

--- run.sh
+++ run.sh
@@ -5,8 +5,3 @@
 # Mail: niu.liu@nju.edu.cn
 # Created Time: Sat 19 Sep 2026 09:07:13 PM CST
 #########################################################################
-# 应无输出
-rg -n '2020-nsfc-key-funding' content/publication
-
-# 查看修改后的项目关联
-git diff -- content/publication/
